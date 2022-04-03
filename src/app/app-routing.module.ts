86957d13711165b738c5b58eb7fa9d55f435dfe5
Kipkorir2017 <Kipkorir2017>
2022-04-03 12:49:36 +0300

    add changes to app module file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { PostComponent } from './components/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostComponent},
+  {path:'landing',component:LandingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
